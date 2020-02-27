e40fd4d6b20ffadb73b397465662dd893dc9e399
Erik Snippe <61537286+ErikSnippe@users.noreply.github.com>
2020-02-27 09:30:22 +0100

    Update hellowWorld.js

--- hellowWorld.js
+++ hellowWorld.js
@@ -6,3 +6,5 @@ git remote add origin https://github.com/ErikSnippe/hello-world.git
 git push -u origin master
 
 Dit zijn wijzigingen van Erik zelf
+Maar misschien moeten we exra wijzigingen toepassen
+Je weet nooit waar dat goed voor is.
